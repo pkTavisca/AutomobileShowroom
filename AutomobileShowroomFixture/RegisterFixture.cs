70f15176e43865bb38b45f81eccdfa450320259e
Pratik Kumar <pkumar@tavisca.com>
2017-08-24 15:52:09 +0530

    Tests for Register

---
+++ AutomobileShowroomFixture/RegisterFixture.cs
@@ -0,0 +1,25 @@
+﻿using Showroom;
+using Showroom.VehicleModels;
+using Xunit;
+
+namespace AutomobileShowroomFixture
+{
+    public class RegisterFixture
+    {
+        [Fact]
+        public void GetTotalSalesForToday_ShouldReturnTodaysSale_WhenValidInput()
+        {
+            Register register = new Register();
+            register.SoldList.Add(new Invoice(new Car() { SellPrice = 10 }, 10));
+            Assert.Equal(10, register.GetTotalSalesForToday());
+        }
+
+        [Fact]
+        public void GetTotalRentedForToday_ShouldReturnTodaysRentedSale_WhenValidInput()
+        {
+            Register register = new Register();
+            register.RentedList.Add(new Invoice(new Car() { LeasePricePerDay = 10 }, 100));
+            Assert.Equal(100, register.GetTotalRentSaleForToday());
+        }
+    }
+}
