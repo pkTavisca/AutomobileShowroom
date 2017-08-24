e0fa838774335de6da8ae25ecd8b6c0789e79f3a
Pratik Kumar <pkumar@tavisca.com>
2017-08-24 15:10:01 +0530

    driving test for manager

--- AutomobileShowroomFixture/ShowroomManagerFixture.cs
+++ AutomobileShowroomFixture/ShowroomManagerFixture.cs
@@ -33,5 +33,31 @@ namespace AutomobileShowroomFixture
 
             Assert.Equal(0, storage.Count());
         }
+
+        [Fact]
+        public void Repairing_a_car()
+        {
+            VehicleStorage storage = new VehicleStorage();
+            Car car = new Car() { Id = 1 };
+            Register register = new Register();
+            ShowroomManager manager = new ShowroomManager(storage, register);
+
+            manager.SendForMaintainence(car, 100);
+
+            Assert.Equal(1, storage.Count());
+        }
+
+        [Fact]
+        public void Test_driving_a_car()
+        {
+            VehicleStorage storage = new VehicleStorage();
+            Car car = new Car() { Id = 1, Name = "Maruti" };
+            Register register = new Register();
+            ShowroomManager manager = new ShowroomManager(storage, register);
+
+            manager.SendForMaintainence(car, 100);
+
+            Assert.Equal("Maruti was test driven.", manager.TestDrive(car));
+        }
     }
 }
